fc4576f9a0d83100f0f0fe072e32e3ad303a7a2a
kinyichukwu <kinyichukwuose@gmail.com>
2022-11-28 00:06:20 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Home from "./routes/home/home";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation";
+import SignIn from "./routes/sign-in/sign-in";
+
+const Shop = ( ) => {
+  return(
+    <div>I am a shop</div>
+  )
+}
 
 function App() {
- 
-  
   return (
     <Routes>
-    <Route path="/home" element= {<Home/>}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Route>
     </Routes>
-    
   );
 }
 
