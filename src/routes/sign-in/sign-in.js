c37f009e0805ec76fc8edc659abb67bc371a3ea1
kinyichukwu <kinyichukwuose@gmail.com>
2022-11-29 00:10:18 +0100

    setting up sign in with firebase

--- src/routes/sign-in/sign-in.js
+++ src/routes/sign-in/sign-in.js
@@ -1,7 +1,26 @@
+import {
+    auth, 
+  signInWithGooglePopUp,
+  signInWithGoogleRedirect,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+
+
+
 const SignIn = () => {
+   
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopUp(auth); 
+
+    const userDocRef = createUserDocumentFromAuth(user);
+  };
+
+  
   return (
     <div>
-      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in button</button>
+      
     </div>
   );
 };
