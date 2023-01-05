2cde86cd36d1b0ae91554efc111a59fb772b51fb
kinyichukwu <kinyichukwuose@gmail.com>
2023-01-05 16:21:51 +0100

    To do list in react, ptaticing react features

--- src/learnMore.js
+++ src/learnMore.js
@@ -1,13 +1,45 @@
 import { useState } from "react";
 
-export const Test = () => {
-  const [age, setAge] = useState(0);
+const Task = ({x, i, deleteTaxFunction}) => {
+  const [color, setColor] = useState('black')
 
-  const addAge = () => setAge((prev) =>!prev);
+  const changeColor= () => setColor((prev) => prev !== 'green'? 'green': 'black')
+  return <div>
+  {" "}
+  <h3 key={i} style={{color: `${color}`}}>
+    {i}. {x.taskName}
+  </h3>
+  <button onClick={changeColor}>Complete</button>
+  <button onClick={() => deleteTaxFunction(x.id)}>x</button>
+</div>
+}
+
+export const Test = () => {
+  const [todoList, settodoList] = useState([]);
+  const [newTax, setnewTax] = useState("");
+
+  const inputTax = (e) => setnewTax(() => e.target.value);
+
+  const AddTodo = () => {
+    const task = {
+      id: todoList.length === 0? 1: todoList[todoList.length - 1].id + 1,
+      taskName: newTax
+    }
+    settodoList((prev) => [...prev, task]);
+  };
+
+  const deleteTax = (taskName) => {
+    settodoList((prev) => prev.filter((todo) => todo.id !== taskName))
+  }
+
   return (
     <>
-      <h1>{age}</h1>
-      <button onClick={addAge}>Clivk me</button>
+      <input type="text" onChange={inputTax} />
+      <button onClick={AddTodo}>Add Tax</button>
+      {todoList.map((x, i) => (
+        <Task x={x} i={i} deleteTaxFunction= {deleteTax}/>
+      ))}
+      <p>{newTax}</p>
     </>
   );
 };
